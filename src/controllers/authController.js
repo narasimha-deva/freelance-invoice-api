fe40205ee3e6694e5c825618e9470471e423945d
Narasimha <chennayagari.narasimha@gmail.com>
2026-03-07 14:14:50 +0530

    Phase 1: Project setup with Express server and folder structure

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,2 +1,13 @@
-registerUser
-loginUser
+import express from "express";
+
+const router = express.Router();
+
+router.post("/register", (req, res) => {
+  res.send("Register route working");
+});
+
+router.post("/login", (req, res) => {
+  res.send("Login route working");
+});
+
+export default router;
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,2 +1,13 @@
-POST /api/auth/register
-POST /api/auth/login
+import express from "express";
+
+const router = express.Router();
+
+router.post("/register", (req, res) => {
+  res.send("Register API working");
+});
+
+router.post("/login", (req, res) => {
+  res.send("Login API working");
+});
+
+export default router;
